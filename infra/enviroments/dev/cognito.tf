38cfeaac440416f10bb643bde8fe98551e627dfc
davidtrajcev <60884704+davidtrajcev@users.noreply.github.com>
2026-03-02 17:48:51 +0100

    Initial commit

---
+++ infra/enviroments/dev/cognito.tf
@@ -0,0 +1,51 @@
+locals {
+  name_prefix = "saas-dev"
+}
+
+data "aws_region" "current" {}
+
+resource "aws_cognito_user_pool" "this" {
+  name = "${local.name_prefix}-users"
+
+  # Keep it simple + secure
+  password_policy {
+    minimum_length    = 12
+    require_lowercase = true
+    require_numbers   = true
+    require_uppercase = true
+    require_symbols   = false
+  }
+
+  # Optional, but nice for SaaS: require verified email
+  auto_verified_attributes = ["email"]
+}
+
+resource "aws_cognito_user_pool_client" "this" {
+  name         = "${local.name_prefix}-web-client"
+  user_pool_id = aws_cognito_user_pool.this.id
+
+  generate_secret = false
+
+  # Simple auth flows for development
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+  ]
+
+  # Keep OAuth off for now (less to configure).
+  # We can add Hosted UI + OAuth later when we add a frontend domain.
+}
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.this.id
+}
+
+output "cognito_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.this.id
+}
+
+# JWT issuer for API Gateway JWT authorizer later
+output "cognito_issuer" {
+  value = "https://cognito-idp.${data.aws_region.current.name}.amazonaws.com/${aws_cognito_user_pool.this.id}"
+}
